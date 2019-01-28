d3602666469b000ceacf8b954ddf062ec96f81db
jdbbaugh <jbaugh78@msn.com>
2019-01-28 15:50:11 -0600

    news displays card of all news and card specific to user

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import NewsCard from './NewsCard'
+import UserNewsCard from './UserNewsCard'
 
 export default class NewsList extends Component {
   render() {
+    const userId = 1
+    const userSaved = this.props.news.filter(newsArticles => newsArticles.userId === userId)
     return(
       <>
       <h1 className="newsHeader">News Today</h1>
@@ -10,6 +13,12 @@ export default class NewsList extends Component {
           {this.props.news.map(newsArticle =>(
             <NewsCard key={newsArticle.id} newsArticle={newsArticle} {...this.props} />
           ))}
+        </section>
+        <h1>Your Saved News</h1>
+        <section>
+            {userSaved.map(userArticle =>(
+              <UserNewsCard newsArticle={userArticle}/>
+            ))}
         </section>
       </>
     )
--- src/components/news/UserNewsCard.js
+++ src/components/news/UserNewsCard.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom"
 
-
-export default class NewsCard extends Component {
-
+export default class UserNewsCard extends Component {
   render() {
     return(
       <>
