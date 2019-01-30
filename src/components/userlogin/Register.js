1f773e82e08dfc26855fbdb526812b46263cb5fa
Asia Carter <asiamcarter@gmail.com>
2019-01-30 11:06:07 -0600

    refactored Register.js code

--- src/components/userlogin/Register.js
+++ src/components/userlogin/Register.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react"
-// import { Link } from "react-router-dom
-
 export default class Register extends Component {
 
     state = {
@@ -13,18 +11,15 @@ export default class Register extends Component {
         const stateToChange= {};
         stateToChange[evt.target.id] = evt.target.value
         this.setState(stateToChange);
-        // console.log(stateToChange)
-    };
+    }
 
     getAllUsers = evt => {
         evt.preventDefault();
        this.props.getAll()
        .then(allUsers => {
            let usersArray = allUsers.filter(user => {
-               console.log(this.state.userName)
                return (user.userName === this.state.userName)
            })
-           console.log("USER ARRAY:", usersArray)
            if (usersArray.length > 0) {
                alert("Sorry, this username is taken!")
            }
@@ -33,18 +28,17 @@ export default class Register extends Component {
               allUsers.forEach(user => {
                 sessionStorage.setItem("User", user.id);
                 let sessionUser = sessionStorage.getItem("User")
-                console.log(sessionUser)
-                  const newUser = {
+                const newUser = {
                     userName: this.state.userName,
                     email: this.state.email,
                     password: this.state.password
-                  }
-                   this.props.addUser(newUser)
-                   this.props.history.push("/")
+                }
+                this.props.addUser(newUser)
+                this.props.history.push("/")
               })
             }
-    })
-   }
+        })
+    }
 
     render() {
         return (
@@ -53,15 +47,15 @@ export default class Register extends Component {
                 <form>
                     <div>
                         <label htmlFor="UserName"> UserName: </label>
-                            <input type="text" required onChange={this.handleFieldChange} id="userName" placeholder="Enter username Here" />
+                        <input type="text" required onChange={this.handleFieldChange} id="userName" placeholder="Enter username Here" />
                     </div>
                     <div>
                         <label htmlFor="Email">Email:</label>
-                            <input type="text" required onChange={this.handleFieldChange} id="email"/>
+                        <input type="text" required onChange={this.handleFieldChange} id="email"/>
                     </div>
                     <div>
                         <label htmlFor="Password">Password:</label>
-                            <input type="text" required onChange={this.handleFieldChange} id="password"/>
+                        <input type="text" required onChange={this.handleFieldChange} id="password"/>
                     </div>
                     <button type="submit" onClick={this.getAllUsers}> Register a new account. </button>
                 </form>
